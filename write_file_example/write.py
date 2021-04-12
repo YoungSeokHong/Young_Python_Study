3e1f9e04f6ce811527aeaeddeecc73bf6259256e
YoungSeok Hong <cevinlion@gmail.com>
2021-04-13 00:52:56 +0900

    Study write_file

---
+++ write_file_example/write.py
@@ -0,0 +1,6 @@
+# with open('write_file_example/new_file.txt', 'w') as f:
+#    f.write('Hello World!\n')
+#    f.write('My name is Young\n')
+
+with open('write_file_example/new_file.txt', 'a') as f:
+   f.write("HoHoHoHoHo\n")
