e1b516ff81650ee5ea98e6e2d5e27f090f7fb089
홍영석 <hong-yeongseog@hong-yeongseog-ui-MacBookAir.local>
2021-04-17 23:07:32 +0900

    Make a wordbook with Python

---
+++ wordbook.py
@@ -0,0 +1,10 @@
+with open('vocabulary.txt', 'a') as f:
+    
+    while True:
+        eng = input("영어 단어를 입력하세요:")
+        if eng == 'q':
+            break
+        kor = input("한국어 뜻을 입력하세요:")
+        if kor == 'q':
+            break
+        f.write(eng + ': ' + kor + '\n')
